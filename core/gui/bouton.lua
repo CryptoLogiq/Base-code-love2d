563521f758bc2b8be27236e4f4ff3f9bfe9fa72b
Crypto Logiq <orbitaldigital@gmail.com>
2026-04-02 09:20:52 +0200

    bouton base

--- core/gui/bouton.lua
+++ core/gui/bouton.lua
@@ -1,55 +1,177 @@
 
 local bouton = {lst_group={}}
 
-local function load()
+local function load(self)
 end
 --
 
-local function update(dt)
+local function getGroup(self)
+  return self.group
 end
 --
 
-local function draw()
+local function setFunction(self, fct)
+  self.fct = fct
 end
 --
 
-local function mousepressed(x,y,button,istouch,presses)
+local function update(self, dt)
+  self.cx = self.x + self.w/2
+  self.cy = self.y + self.h/2
+  --
+  self.oy = 0 - (self.h/2)
+  --
+  local txt = self.text
+  txt.ox = txt.w/2
+  txt.oy = txt.h/2
+  txt.x = self.cx - txt.ox
+  txt.y = self.cy - txt.oy
 end
 --
 
-local function keypressed(k,s,isrepeat)
+local function draw(self)
+  love.graphics.setColor(self.color)
+  love.graphics.rectangle("fill", self.x, self.y, self.w, self.h, self.rounded)
+  --
+  local txt = self.text
+  love.graphics.setColor(txt.color)
+  love.graphics.draw(txt.data, txt.x, txt.y)
 end
 --
 
-function bouton.new(text, x, y, w, h, group)
-  local new = {text=text, x=x, y=y, w=w, h=h, load=load, update=update, draw=draw, mousepressed=mousepressed, keypressed=keypressed}
+local function mousepressed(self)
+  if self.fct then
+    self:fct()
+  end
+end
+--
 
+local function keypressed(self)
+  if self.fct then
+    self.fct(self)
+  end
+end
+--
+
+--## Boutons ## :
+
+function bouton.new(text, x, y, w, h, group, font)
+  local usedFont = font
+  if not font then
+    usedFont = love.graphics.getFont()
+  end
+  --
+  local new = {textString=text, x=x, y=y, w=w, h=h, rounded=15, rotate=0, sx=1, sy=1, ox=0, oy=0, color={1,1,1,1}, load=load, update=update, draw=draw, getGroup=getGroup}
+  new.cx = new.w/2
+  new.cy = new.h/2
+  --
+  new.text = {data=love.graphics.newText(usedFont, new.textString),  x=0, y=0, w=0, h=0, ox=0, oy=0, color={0,0,0,1}, colorDefaut={0,0,0,1}}
+  new.text.w,new.text.h = new.text.data:getDimensions()
+  --
+  new.fct = function(self) return print(new.textString, "function not exit") end
+  --
   if group then
-    bouton.addGroup(group, new)
+    if type(group) == "table" then
+      new.group = group
+
+    elseif type(group) == "string" then
+      new.group = bouton.addGroupName(group, new)
+    end
   end
   return new
 end
 --
 
+-- ## Groups ## --
+
+local function groupLoad(group)
+  for n=1, #group do
+    local bt = group[n]
+    bt:load()
+  end
+end
+--
+
+local function groupUpdate(group, dt)
+  for n=1, #group do
+    local bt = group[n]
+    bt:update(dt)
+  end
+end
+--
+
+local function groupDraw(group)
+  for n=1, #group do
+    local bt = group[n]
+    bt:draw()
+  end
+end
+--
+
+local function groupMousepressed(group, x,y,button,istouch,presses)
+  for n=1, #group do
+    local bt = group[n]
+    if x >= bt.x and y >= bt.y and x <= bt.x + bt.w and y <= bt.y + bt.h then
+      bt:fct()
+      break
+    end
+  end
+end
+--
+
+local function groupKeypressed(group, k,s,isrepeat)
+  for n=1, #group do
+    local bt = group[n]
+  end
+end
+--
+
 function bouton.newGroup(name)
-  bouton.lst_group[name] = {}
+  bouton.lst_group[name] = {load=groupLoad, update=groupUpdate, draw=groupDraw, mousepressed=groupMousepressed, keypressed=groupKeypressed}
+  return bouton.lst_group[name]
 end
 --
 
 function bouton.addGroupName(groupName, bt)
+  local groupExist = false
+  local gr = nil
+  --
   if not bouton.lst_group.groupName then
-    if type(groupName) == "text" then
-      bouton.newGroup(groupName)
-    else
-      print('bad argument, you need create a group with "text variable", use before : bouton.newGroup(name)')
-      return nil
+
+    if type(groupName) == "string" then
+
+      if bouton.lst_group[groupName] then
+        gr = bouton.lst_group[groupName]
+      else
+        gr = bouton.newGroup(groupName)
+      end
+      groupExist = true
+
+    elseif type(groupName) == "table" then
+
+      for index, groups in pairs(bouton.lst_group) do
+        if groupName == groups then
+          gr = groups
+          groupExist = true
+          break
+        end
+      end
+
     end
+
   end
   --
-  local gr = bouton.lst_group[groupName]
+  if not groupExist then
+    print('bad argument with', groupName)
+    print('You need create a group with "text variable" OR "group table" use before : bouton.newGroup(name)')
+    return nil
+  end
+  --
   local id = #gr + 1
   bt.id = id
   table.insert(gr, bt)
+  bt.group = gr
+  return gr
 end
 --
 
--- scenes/scene_menu.lua
+++ scenes/scene_menu.lua
@@ -1,19 +1,26 @@
 
 local menu = Scene.new("Menu")
 
-local nav = {
-  main={name="main"},
-  quit={name="quit game", txt="Press Enter to Exit Game or Escape to Abort."}
-}
+--
+local nav = {}
+nav.group = Gui.bouton.newGroup("Menu")
+print("nav.group : ", nav.group)
+nav.main = Gui.bouton.new("Play", 150, 200, 500, 50, "Menu")
+nav.quit = Gui.bouton.new("Quit", 150, 500, 500, 50, "Menu")
+
+--
 menu.current = nav.main
+
 --
 
 function menu.load()
   menu.current = nav.main
+  nav.group:load()
 end
 --
 
 function menu.update(dt)
+  nav.group:update(dt)
 end
 --
 
@@ -21,14 +28,18 @@ function menu.draw()
   if menu.current == nav.quit then
     love.graphics.printf(nav.quit.txt,0,love.graphics.getHeight()/2,love.graphics.getWidth(),"center",0,2,2,love.graphics.getWidth()/4)
   end
+  nav.group:draw()
 end
 --
 
 function menu.mousepressed(x,y,button,istouch,presses)
+  nav.group:mousepressed(x,y,button,istouch,presses)
 end
 --
 
 function menu.keypressed(k,s,isrepeat)
+  nav.group:keypressed(k,s,isrepeat)
+  --
   if k == "escape" then
     if menu.current == nav.main then
       menu.current = nav.quit
